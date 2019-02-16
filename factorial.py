10b4d22237187de711aad26c86300c831582590f
Abhishek15102 <44195699+Abhishek15102@users.noreply.github.com>
2019-02-17 00:20:57 +0530

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,5 @@
+name=input("Enter your name ")
+number=int(input("Enter the number for factorial"))
+if number=0:
+    print(name,"your number is Zero \nwe learn feom child hood if we try to find factorial of zero we factorial is return 0:)")
+if n
