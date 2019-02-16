483074979493140f7dcaa21269ab8f425808777a
Abhishek15102 <44195699+Abhishek15102@users.noreply.github.com>
2019-02-17 01:12:31 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,23 @@
-name=input("Enter your name ")
-number=int(input("Enter the number for factorial"))
-if number=0:
-    print(name,"your number is Zero \nwe learn feom child hood if we try to find factorial of zero we factorial is return 0:)")
-if n
+fact=1
+name=input("Enter your name-")
+
+print('\n\nHello '+name,'Happy coading life')
+
+number=int(input("enter number for factorial-"))
+
+if number==0:
+    
+    print(name,"factorial of",number,"is 1")
+    
+elif number<0:
+    
+    
+    print(name,"Do not enter (-ve) number")
+    
+else:
+    for i in range(1,number+1):
+        fact=fact*i
+    print('\nFactorial of',number,'=',fact)
+    
+    print("\nDont take much stress coading is to easy:)")
+        
